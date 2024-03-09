3946cc3f58d4f8050ed8ed99ee33d787738e1c84
maraoai <amerneedi@gmail.com>
2024-03-08 23:11:22 -0800

    update input schema

--- input_schema.py
+++ input_schema.py
@@ -4,5 +4,11 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["Penguins having dinner"]
+    },
+    "num_inference_steps": {
+        'datatype': 'INT',
+        'required': True,
+        'shape': [1],
+        'example': [40]
     }
 }
